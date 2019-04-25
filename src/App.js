d2d888bc4bbe9fd85b47e98428d9bb6c9ab288ad
snabar <snabar@wsgc.com>
2019-04-25 08:31:13 -0700

    Add page not found basic function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import Greetings from "./components/Greetings";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import WorkHistory from "./components/WorkHistory";
 import Home from "./components/Home";
@@ -13,10 +13,17 @@ class App extends Component {
       <BrowserRouter>
         <div className="container">
           <Nav />
-          <Route exact path="/" component={Home} />
-          <Route path="/greetings" component={Greetings} />
-          <Route path="/search" component={SearchBar} />
-          <Route path="/work" component={WorkHistory} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/greetings" component={Greetings} />
+            <Route path="/search" component={SearchBar} />
+            <Route path="/work" component={WorkHistory} />
+            <Route
+              render={() => {
+                return <p>Not found</p>;
+              }}
+            />
+          </Switch>
         </div>
       </BrowserRouter>
     );
